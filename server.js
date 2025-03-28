83d2967cd5b21a03f29c6036feb2a706feca696c
Felipe Gonzalez <contraviento@gmail.com>
2025-03-27 19:48:42 -0700

    create userschema for mongo db

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
 	res.send('Home Page');
 });
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 mongoose
 	.connect(process.env.MONGO_URI)
